a465a804dbe5fa3290c2cd63e6fe7669bd459ac8
N-Masi <nickmasi17@gmail.com>
2025-07-19 16:07:18 -0400

    data: include India as territory

--- src/safe_earth/data/strata/generate_gdf.py
+++ src/safe_earth/data/strata/generate_gdf.py
@@ -2,5 +2,8 @@ from pygeoboundaries_geolab import get_gdf
 import geopandas as gpd
 
 gdf = get_gdf('ALL', ['UNSDG-subregion', 'worldBankIncomeGroup', 'maxAreaSqKM'])
-gdf = gdf.drop(['shapeISO'], axis=1)
-gdf.to_csv('safe/data/strata/gdf_territory_region_income.csv', index=False)
+gdf = gdf.drop(columns=['shapeISO', 'shapeID', 'shapeGroup', 'shapeType'])
+gdf = gpd.GeoDataFrame(gdf, geometry=gdf['geometry'])
+gdf = gdf.set_geometry('geometry').set_crs(4326)
+gdf.to_csv('src/safe_earth/data/strata/gdf_territory_region_income.csv', index=False)
+gdf.to_file('src/safe_earth/data/strata/territory_region_income.geojson', driver='GeoJSON')
